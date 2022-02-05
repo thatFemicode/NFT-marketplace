5e73b658c44a80df833c23e517ec5bd948f4fba6
thatFemicode <leyedavid10@gmail.com>
2022-02-05 23:44:02 +0100

    celebrity nft images

---
+++ src/Components/CelebrityArt/data.js
@@ -0,0 +1,157 @@
+import Drake from "./Celebrity-NFT/Drake.png";
+import Eminem from "./Celebrity-NFT/Eminem.jpg";
+import Tyler from "./Celebrity-NFT/Feliciathegoat.jpg";
+import Frank from "./Celebrity-NFT/Frank Ocean.png";
+import IceCube from "./Celebrity-NFT/Ice Cube.png";
+import Jcole from "./Celebrity-NFT/J Cole.jpg";
+import Jason from "./Celebrity-NFT/Jason Derulo.jpg";
+import Juice from "./Celebrity-NFT/Juice.png";
+import Kanye from "./Celebrity-NFT/Kanye.jpg";
+import Kendrick from "./Celebrity-NFT/Kendrick Lamar.png";
+import LilNas from "./Celebrity-NFT/LilNas.jpg";
+import Nicki from "./Celebrity-NFT/Nicki Minaj.jpg";
+import Nipsey from "./Celebrity-NFT/Nipsey Hussle.jpg";
+import PopSmoke from "./Celebrity-NFT/Pop Smoke.jpg";
+import PostMalone from "./Celebrity-NFT/Post Malone.jpg";
+import Rema from "./Celebrity-NFT/Rema.jpg";
+import Rihana from "./Celebrity-NFT/Rihanna.jpg";
+import Snoop from "./Celebrity-NFT/Snoop Dogg final.png";
+import TheWeekend from "./Celebrity-NFT/The Weeknd.jpg";
+import Travis from "./Celebrity-NFT/Travis Scott.png";
+import Avatar from "./Celebrity-NFT/avatar.png";
+const items = [
+  { id: 1, title: "Apple", artist: "Femi", artistImage: Artist, image: Apple },
+  {
+    id: 2,
+    title: "Avocado",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Avocado,
+  },
+  {
+    id: 3,
+    title: "Labador",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Labador,
+  },
+  {
+    id: 4,
+    title: "Bruno Mars",
+    artist: "Femi",
+    artistImage: Artist,
+    image: BrunoMars,
+  },
+  {
+    id: 5,
+    title: "Bull Dog",
+    artist: "Femi",
+    artistImage: Artist,
+    image: BullDog,
+  },
+  {
+    id: 6,
+    title: "Chris Evans",
+    artist: "Femi",
+    artistImage: Artist,
+    image: ChrisEvans,
+  },
+  {
+    id: 7,
+    title: "Chris HemsWorth",
+    artist: "Femi",
+    artistImage: Artist,
+    image: ChrisHemsWorth,
+  },
+  {
+    id: 8,
+    title: "Crushed Can",
+    artist: "Femi",
+    artistImage: Artist,
+    image: CrushedCan,
+  },
+  {
+    id: 9,
+    title: "Deer",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Deer,
+  },
+  {
+    id: 10,
+    title: "Digital Bust",
+    artist: "Femi",
+    artistImage: Artist,
+    image: DigitalBust,
+  },
+  {
+    id: 11,
+    title: "Distortion",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Distortion,
+  },
+  {
+    id: 12,
+    title: "Elephant",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Elephant,
+  },
+  {
+    id: 13,
+    title: "Flamingo",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Flamingo,
+  },
+  {
+    id: 14,
+    title: "German Sheppard",
+    artist: "Femi",
+    artistImage: Artist,
+    image: GermanShepperd,
+  },
+  {
+    id: 15,
+    title: "Gobiya",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Gobiya,
+  },
+  {
+    id: 16,
+    title: "GoldenEagle",
+    artist: "Femi",
+    artistImage: Artist,
+    image: GoldenEagle,
+  },
+  {
+    id: 17,
+    title: "GOT",
+    artist: "Femi",
+    artistImage: Artist,
+    image: GOT,
+  },
+  {
+    id: 18,
+    title: "Harry Porter",
+    artist: "Femi",
+    artistImage: Artist,
+    image: HarryPorter,
+  },
+  {
+    id: 19,
+    title: "Holy",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Holy,
+  },
+  {
+    id: 20,
+    title: "Human Art",
+    artist: "Femi",
+    artistImage: Artist,
+    image: HumanArt,
+  },
+];
