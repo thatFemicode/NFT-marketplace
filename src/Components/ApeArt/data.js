0f75fdf1bf7d02f3989ca692c119105189616b55
thatFemicode <leyedavid10@gmail.com>
2022-02-06 01:30:38 +0100

    ape art

---
+++ src/Components/ApeArt/data.js
@@ -0,0 +1,179 @@
+import Basketball from "./Ape-NFT/BasketballBoredApe.png";
+import Bored from "./Ape-NFT/BoredApe.png";
+import Captain from "./Ape-NFT/CaptainBoredApe.png";
+import Chainz from "./Ape-NFT/ChainzBoredApe.png";
+import ColdBlodded from "./Ape-NFT/ColdbloodedBoredApe.jpg";
+import Cyclops from "./Ape-NFT/CyclopsBoredApe.jpg";
+import Famous from "./Ape-NFT/FamousBoredApe.jpg";
+import Fashion from "./Ape-NFT/FashionBoredApe.jpg";
+import Football from "./Ape-NFT/FootballBoredApe.webp";
+import Happy from "./Ape-NFT/HappyBeastBoredApe.png";
+import Jordan from "./Ape-NFT/JordanBoredApe.jpg";
+import Lover from "./Ape-NFT/LoverBoredApe.png";
+import Machine from "./Ape-NFT/MachineBoredApe.png";
+import Monk from "./Ape-NFT/MonkBoredApe.jpg";
+import Nanny from "./Ape-NFT/NannyBoredApe.jpg";
+import Obsidian from "./Ape-NFT/ObsidianBoredApe.jpg";
+import Old from "./Ape-NFT/OldBoredApe.png";
+import Space from "./Ape-NFT/SpaceBoredApe.png";
+import Surprised from "./Ape-NFT/SurprisedBoredApe.png";
+import Talent from "./Ape-NFT/TalentBored.png";
+import Weed from "./Ape-NFT/WeedBoredApe.png";
+import Yatch from "./Ape-NFT/YachtBoredApe.jpg";
+import Artist from "./Celebrity-NFT/avatar.png";
+const items = [
+  {
+    id: 1,
+    title: "Suprised Ape",
+    artist: "David",
+    artistImage: Artist,
+    image: Surprised,
+  },
+  {
+    id: 2,
+    title: "Basketball Ape",
+    artist: "David",
+    artistImage: Artist,
+    image: Basketball,
+  },
+  {
+    id: 3,
+    title: "Bored Ape",
+    artist: "David",
+    artistImage: Artist,
+    image: Bored,
+  },
+  {
+    id: 4,
+    title: "Captain Ape",
+    artist: "David",
+    artistImage: Artist,
+    image: Captain,
+  },
+  {
+    id: 5,
+    title: "2Chainz Ape",
+    artist: "David",
+    artistImage: Artist,
+    image: Chainz,
+  },
+  {
+    id: 6,
+    title: "ColdBlodded Ape",
+    artist: "David",
+    artistImage: Artist,
+    image: ColdBlodded,
+  },
+  {
+    id: 7,
+    title: "Cyclops Ape",
+    artist: "David",
+    artistImage: Artist,
+    image: Cyclops,
+  },
+  {
+    id: 8,
+    title: "Famous Ape",
+    artist: "David",
+    artistImage: Artist,
+    image: Famous,
+  },
+  {
+    id: 9,
+    title: "Fashion Ape",
+    artist: "David",
+    artistImage: Artist,
+    image: Fashion,
+  },
+  {
+    id: 10,
+    title: "Football Ape",
+    artist: "David",
+    artistImage: Artist,
+    image: Football,
+  },
+  {
+    id: 11,
+    title: "Happy Ape",
+    artist: "David",
+    artistImage: Artist,
+    image: Happy,
+  },
+  {
+    id: 12,
+    title: "Jordan Ape",
+    artist: "David",
+    artistImage: Artist,
+    image: Jordan,
+  },
+  {
+    id: 13,
+    title: "Lover Ape",
+    artist: "David",
+    artistImage: Artist,
+    image: Lover,
+  },
+  {
+    id: 14,
+    title: "Machine Ape",
+    artist: "David",
+    artistImage: Artist,
+    image: Machine,
+  },
+  {
+    id: 15,
+    title: "Monk Ape",
+    artist: "David",
+    artistImage: Artist,
+    image: Monk,
+  },
+  {
+    id: 16,
+    title: "Nanny Ape",
+    artist: "David",
+    artistImage: Artist,
+    image: Nanny,
+  },
+  {
+    id: 17,
+    title: "Obsidian Ape",
+    artist: "David",
+    artistImage: Artist,
+    image: Obsidian,
+  },
+  {
+    id: 18,
+    title: "Old Ape",
+    artist: "David",
+    artistImage: Artist,
+    image: Old,
+  },
+  {
+    id: 19,
+    title: "Space Ape",
+    artist: "David",
+    artistImage: Artist,
+    image: Space,
+  },
+  {
+    id: 20,
+    title: "Talent Ape",
+    artist: "David",
+    artistImage: Artist,
+    image: Talent,
+  },
+  {
+    id: 21,
+    title: "High Ape",
+    artist: "David",
+    artistImage: Artist,
+    image: Weed,
+  },
+  {
+    id: 22,
+    title: "Yatch Ape",
+    artist: "David",
+    artistImage: Artist,
+    image: Yatch,
+  },
+];
