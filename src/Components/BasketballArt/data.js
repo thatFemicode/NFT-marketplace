bfa4ff9bff085c24b33ded51d1900c1289bf4126
thatFemicode <leyedavid10@gmail.com>
2022-02-06 01:48:36 +0100

    basketball nft

---
+++ src/Components/BasketballArt/data.js
@@ -0,0 +1,243 @@
+import Caruso from "./Basketball-NFT/Alex Caruso.jpg";
+import Ben from "./Basketball-NFT/Ben Simmons.jpg";
+import ChrisPaul from "./Basketball-NFT/Chris Paul.jpg";
+import Dennis from "./Basketball-NFT/Dennis Schroder.jpg";
+import Derrick from "./Basketball-NFT/Derrick Rose 🌹.jpg";
+import Booker from "./Basketball-NFT/Devin Booker.jpg";
+import Mitchell from "./Basketball-NFT/Donovan Mitchell.jpg";
+import Wade from "./Basketball-NFT/Dwayne Wade.jpg";
+import Giannis from "./Basketball-NFT/Giannis Antetokounmpo 🏛.jpg";
+import Hakeem from "./Basketball-NFT/Hakeem Olajuwon 🚀.jpg";
+import Ja from "./Basketball-NFT/Ja Morant.jpg";
+import Jaylen from "./Basketball-NFT/Jaylen Brown.jpg";
+import Jayson from "./Basketball-NFT/Jayson Tatum 0️⃣.jpg";
+import Jimmy from "./Basketball-NFT/Jimmy Butler 🤠.jpg";
+import Joel from "./Basketball-NFT/Joel Embiid 🔨.jpg";
+import Kevin from "./Basketball-NFT/Kevin Durant 💸.jpg";
+import Lebron from "./Basketball-NFT/King James 👑.jpg";
+import Klay from "./Basketball-NFT/Klay Thompson.jpg";
+import Kobe from "./Basketball-NFT/Kobe Bryant.jpg";
+import Kyrie from "./Basketball-NFT/Kyrie Irving.jpg";
+import Luka from "./Basketball-NFT/Luka Doncic.jpg";
+import Manu from "./Basketball-NFT/Manu Ginobilli 🇦🇷.jpg";
+import Gasol from "./Basketball-NFT/Marc Gasol.jpg";
+import Siakam from "./Basketball-NFT/Pascal Siakam 🦖.jpg";
+import Paul from "./Basketball-NFT/Paul George 🎮.jpg";
+import Ricky from "./Basketball-NFT/Ricky Rubio 🇪🇸.jpg";
+import Steph from "./Basketball-NFT/Steph Curry 👨🏽_🍳.jpg";
+import Steve from "./Basketball-NFT/Steve Kerr.jpg";
+import Trae from "./Basketball-NFT/Trae Young.jpg";
+import Zach from "./Basketball-NFT/Zach Lavine 🐂.jpg";
+import Artist from "./Celebrity-NFT/avatar.png";
+const items = [
+  {
+    id: 1,
+    title: "Alex Caruso",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Caruso,
+  },
+  {
+    id: 2,
+    title: "Ben Simmons",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Ben,
+  },
+  {
+    id: 3,
+    title: "Chris Paul",
+    artist: "Femi",
+    artistImage: Artist,
+    image: ChrisPaul,
+  },
+  {
+    id: 4,
+    title: "Dennis Schroder",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Dennis,
+  },
+  {
+    id: 5,
+    title: "Derrick Rose",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Derrick,
+  },
+  {
+    id: 6,
+    title: "Devin Booker",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Booker,
+  },
+  {
+    id: 7,
+    title: "Donovan Mitchell",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Mitchell,
+  },
+  {
+    id: 8,
+    title: "Dwayne Wade",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Wade,
+  },
+  {
+    id: 9,
+    title: "Giannia Antetokounmpo",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Giannis,
+  },
+  {
+    id: 10,
+    title: "Hakeem Olajuwon",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Hakeem,
+  },
+  {
+    id: 11,
+    title: "Ja Morant",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Ja,
+  },
+  {
+    id: 12,
+    title: "Jaylen Brown",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Jaylen,
+  },
+  {
+    id: 13,
+    title: "Jayson Tatum",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Jayson,
+  },
+  {
+    id: 14,
+    title: "Jimmy Butler",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Jimmy,
+  },
+  {
+    id: 15,
+    title: "Joel Embid",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Joel,
+  },
+  {
+    id: 16,
+    title: "Kevin Durant",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Kevin,
+  },
+  {
+    id: 17,
+    title: "Lebron James",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Lebron,
+  },
+  {
+    id: 18,
+    title: "Klay Thompson",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Klay,
+  },
+  {
+    id: 19,
+    title: "Kobe Bryant",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Kobe,
+  },
+  {
+    id: 20,
+    title: "Kyrie Irving",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Kyrie,
+  },
+  {
+    id: 21,
+    title: "Luka Doncid",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Luka,
+  },
+  {
+    id: 22,
+    title: "Manu Ginobli",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Manu,
+  },
+  {
+    id: 23,
+    title: "Marc Gasol",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Gasol,
+  },
+  {
+    id: 24,
+    title: "Pascal Siakam",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Siakam,
+  },
+  {
+    id: 25,
+    title: "Paul George",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Paul,
+  },
+  {
+    id: 26,
+    title: "Ricky Rubio",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Ricky,
+  },
+  {
+    id: 27,
+    title: "Stephen Curry",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Steph,
+  },
+  {
+    id: 28,
+    title: "Steve Kerr",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Steve,
+  },
+  {
+    id: 29,
+    title: "Trae Young",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Trae,
+  },
+  {
+    id: 30,
+    title: "Zach Lavine",
+    artist: "Femi",
+    artistImage: Artist,
+    image: Zach,
+  },
+];
