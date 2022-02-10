a4536f0fe2e41bd0c90c48596ec37c059fef2bb8
thatFemicode <leyedavid10@gmail.com>
2022-02-10 22:20:53 +0100

    about nav test

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 import React, { forwardRef } from "react";
-import { NavbarStyled } from "./NavbarStyled";
+import { NavbarStyled, Container, DropdownStyles } from "./NavbarStyled";
 import { OuterLayout } from "../../Layout/Layout";
 // import logo from "./img/GSW.svg";
 import logo from "./img/slack.svg";
 import { DropdownProvider, DropdownOption, DropdownRoot } from "./Dropdown";
-
+import About from "./Content/About/About";
+import Collection from "./Content/Collections/Collection";
+import Why from "./Content/WhyExpress/Why";
 import { Link } from "react-router-dom";
 // import { useGlobalContext } from "../../Context/context.js";
 
@@ -17,6 +19,24 @@ const Navbar = forwardRef(({ children }, ref) => {
             <Link to="/">
               <img src={logo} alt="" />
             </Link>
+          </div>
+          <div className="options">
+            <DropdownProvider>
+              <DropdownStyles>
+                <Container>
+                  <ul>
+                    <li>
+                      <DropdownOption
+                        name="About"
+                        content={About}
+                        backgroundHeight={286}
+                      />
+                    </li>
+                  </ul>
+                </Container>
+                <DropdownRoot />
+              </DropdownStyles>
+            </DropdownProvider>
           </div>
           <div className="hashtag">
             <Link to="/input">Get Started</Link>
--- src/Components/Navbar/NavbarStyled.js
+++ src/Components/Navbar/NavbarStyled.js
@@ -91,3 +91,74 @@ export const Buttons = styled.button`
     display: none;
   }
 `;
+export const Container = styled.nav`
+  /* background: linear-gradient(150deg, #53f 15%, #05d5ff); */
+
+  > ul {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    /* padding: 0 21px; */
+  }
+`;
+export const DropdownStyles = styled.div`
+  .dropdown-option {
+    /* padding: 20px 25px; */
+    outline: 0;
+    color: #000;
+    font-size: 18px;
+    border: none;
+    transition: opacity 0.2s;
+    background: transparent;
+    &:hover,
+    &:focus {
+      opacity: 0.55;
+    }
+  }
+
+  .dropdown-root {
+    z-index: 10;
+    position: absolute;
+  }
+
+  .dropdown-arrow {
+    position: relative;
+
+    &::before {
+      content: "";
+      position: absolute;
+      width: 16px;
+      height: 16px;
+      background: #fff;
+
+      top: -6.5px;
+      left: -8px;
+      border-radius: 4px 0 0 0;
+
+      transform: rotate(45deg);
+    }
+  }
+
+  .dropdown-container {
+    position: absolute;
+    overflow: hidden;
+    box-shadow: 0 50px 100px -20px rgba(50, 50, 93, 0.25),
+      0 30px 60px -30px rgba(0, 0, 0, 0.3),
+      0 -18px 60px -10px rgba(0, 0, 0, 0.025);
+
+    background: #fff;
+    border-radius: 4px;
+  }
+
+  .dropdown-section {
+    position: absolute;
+  }
+
+  .dropdown-background {
+    position: absolute;
+    bottom: 0;
+    background: #f6f9fc;
+    width: 100%;
+  }
+`;
