c081838c73bf88d1a2d5a5e96cb624d5e4b69695
thatFemicode <leyedavid10@gmail.com>
2022-02-11 22:49:54 +0100

    about nav

--- src/Components/Navbar/Content/About/About.jsx
+++ src/Components/Navbar/Content/About/About.jsx
@@ -2,7 +2,56 @@ import React from "react";
 import { AboutStyled } from "./AboutStyled";
 
 const About = () => {
-  return <AboutStyled></AboutStyled>;
+  return (
+    <AboutStyled>
+      <div className="primary">
+        <span className="documentation" />
+
+        <div>
+          <h3>About ExpressSea</h3>
+          <p>The best digital market to get mind blowing digital art. </p>
+          <p>Connecting your wallet is so easy with the metamask. </p>
+          <p>ExpressSea provides rare digital arts that sways individuals. </p>
+          <div>
+            <ul>
+              <li>
+                <h4>Comece já</h4>
+              </li>
+              <li>Checkout pré-integrado</li>
+              <li>Bibliotecas e SDKs</li>
+              <li>Plugins</li>
+              <li>Exemplos de código</li>
+            </ul>
+
+            <ul>
+              <li>
+                <h4>Guias</h4>
+              </li>
+              <li>Aceite pagamentos online</li>
+              <li>Gerencie assinaturas</li>
+              <li>Envie repasses</li>
+              {/* <li>Implemente pagamentos presenciais</li> */}
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <ul className="secondary">
+        <li>
+          <span className="api-reference" />
+          Referência completa da API
+        </li>
+        <li>
+          <span />
+          Status da API
+        </li>
+        <li>
+          <span />
+          Código aberto
+        </li>
+      </ul>
+    </AboutStyled>
+  );
 };
 
 export default About;
--- src/Components/Navbar/Content/About/AboutStyled.js
+++ src/Components/Navbar/Content/About/AboutStyled.js
@@ -1,3 +1,81 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+const h3 = css`
+  font-size: 0.7rem;
+  font-weight: bold;
+  text-transform: uppercase;
+  color: #32325d;
+  text-align: left;
+`;
+const h2 = css`
+  font-size: 0.9rem;
+  font-weight: bold;
+  color: #32325d;
+  /* text-align: left; */
+`;
 
-export const AboutStyled = styled.div``;
+const p = css`
+  color: #6b7c93;
+  font-size: 0.8rem;
+  text-align: left;
+`;
+
+const hover = css`
+  cursor: pointer;
+  transition: opacity 0.1s ease-in;
+  &:hover {
+    opacity: 0.7;
+  }
+`;
+export const AboutStyled = styled.div`
+  padding: 32px 35px;
+  white-space: nowrap;
+  .primary {
+    display: flex;
+
+    > span {
+      display: inline-block;
+      width: 14px;
+      height: 14px;
+      margin-right: 12px;
+      background: #505e7d;
+      border-radius: 3.5px;
+    }
+
+    > div {
+      > h3 {
+        ${h3}
+        ${hover}
+      }
+      p {
+        ${p}
+        margin-top: 5.7px;
+        max-width: 50px;
+      }
+
+      > div {
+        margin-top: 35px;
+        display: flex;
+        align-items: center;
+
+        > ul + ul {
+          margin-left: 41px;
+        }
+        > ul li {
+          ${hover}
+          color: #424770;
+
+          h4 {
+            color: #8898aa;
+            text-transform: uppercase;
+            text-align: left;
+          }
+
+          & + li {
+            margin-top: 13px;
+            text-align: left;
+          }
+        }
+      }
+    }
+  }
+`;
