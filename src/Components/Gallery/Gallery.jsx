ca661b86e13a12664e87ed8fe292df9f74ed51f2
thatFemicode <leyedavid10@gmail.com>
2022-02-13 23:50:21 +0100

    react transition group package

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -6,29 +6,12 @@ const Gallery = () => {
   return (
     <GalleryStyled>
       <OuterLayout>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam
-        consequatur voluptatibus ipsam dolor deserunt eius voluptas voluptatum
-        neque alias, eligendi dolorem atque molestiae tempora libero illo ipsa
-        quis ad? Error perferendis minima veniam, eveniet deserunt minus
-        accusamus id dolorem praesentium porro, exercitationem repudiandae iste?
-        Quis unde quia quisquam tenetur veritatis temporibus pariatur atque,
-        aspernatur ex veniam enim eligendi sit iure. Eveniet dicta tenetur saepe
-        eius rem deserunt aspernatur quaerat, quas nulla velit cumque obcaecati
-        illo natus ipsam corporis dolores deleniti officia aliquam doloremque.
-        Nemo ad quia ex et, magni, debitis doloribus maiores exercitationem vero
-        delectus vel asperiores esse? In deserunt pariatur delectus amet
-        consequatur dignissimos porro quidem, illo ex atque sapiente,
-        repellendus reprehenderit quasi, facilis nam tenetur id? Aliquam,
-        inventore aliquid? Ex non laborum incidunt voluptates repellendus sequi.
-        Neque aut earum pariatur, veniam perspiciatis consequatur aperiam
-        doloremque minus atque voluptas corrupti consectetur qui odio facilis
-        reprehenderit, itaque ea officia nesciunt odit natus eveniet explicabo.
-        Similique eligendi expedita facilis laboriosam quo ullam eius ducimus
-        pariatur. Ex incidunt praesentium esse doloribus nam minima quo error
-        vitae, nisi illum eaque totam quod amet commodi mollitia quisquam
-        blanditiis, nostrum fugit harum obcaecati neque ipsum corporis. Saepe
-        quod laudantium aspernatur sint natus perspiciatis, repudiandae
-        mollitia.
+        <div className="gallery">
+          <h1>
+            Popular <span> NFTs</span> on{" "}
+            <span className="express">ExpressSea</span>
+          </h1>
+        </div>
       </OuterLayout>
     </GalleryStyled>
   );
--- src/Components/Gallery/GalleryStyled.js
+++ src/Components/Gallery/GalleryStyled.js
@@ -7,4 +7,43 @@ export const GalleryStyled = styled.section`
   padding-left: 2rem;
   padding-right: 2rem;
   padding-top: 4rem;
+  .gallery {
+    h1 {
+      max-width: 600px;
+      font-weight: 600;
+      font-size: 2.7rem;
+      line-height: 1.6;
+      text-align: left;
+      /* color: transparent; */
+
+      span {
+        color: #beaef0;
+        &.express {
+          color: #b06c49;
+        }
+      }
+      @media (max-width: ${({ theme }) => theme.dont}) {
+        font-size: 2.6rem;
+      }
+      @media (max-width: ${({ theme }) => theme.text}) {
+        font-size: 2.2rem;
+      }
+      @media (max-width: ${({ theme }) => theme.desktop}) {
+        font-size: 2.2rem;
+      }
+      @media (max-width: ${({ theme }) => theme.make}) {
+        font-size: 1.8rem;
+      }
+      @media (max-width: ${({ theme }) => theme.max}) {
+        font-size: 1.5rem;
+        max-width: 400px;
+      }
+      @media (max-width: ${({ theme }) => theme.foot}) {
+        font-size: 1.4rem;
+      }
+      @media (max-width: ${({ theme }) => theme.vSmall}) {
+        font-size: 1.2rem;
+      }
+    }
+  }
 `;
